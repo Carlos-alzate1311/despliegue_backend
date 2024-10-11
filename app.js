fcd43340a3b72d4902669088bc1263071ba4d609
carlos-2022 <carlos.alzateg131122@gmail.com>
2024-10-10 23:02:54 -0500

    modificacion app.js

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ app.use(express.json());
 app.use(cors({origin:'*'}))
 app.use('/api/usuarios',require('./routes/usuario.routes'));
 app.use('/api/empleados',require('./routes/empleado.routes'));
-app.listen(3005, ()=>{
-    console.log("El servidor esta en el puerto 3005")
-})
+const PORT = process.env.PORT || 3005; // Deja 3005 como valor predeterminado para pruebas locales
+app.listen(PORT, () => {
+    console.log(`El servidor está en el puerto ${PORT}`);
+});
 
 dbconnect();  
 
